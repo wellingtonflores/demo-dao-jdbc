af5562ed3959748a59c7f14f59c524940530e1eb
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2024-10-08 08:10:33 -0300

    department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+        Department department = new Department(1, "Books");
+        System.out.println(department);
+    }
+}
