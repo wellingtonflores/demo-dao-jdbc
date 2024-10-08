bbf8e2287e240aefe598cb68db068655d249c06e
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2024-10-08 08:28:51 -0300

    SellerDaoJDBC e DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,8 +10,11 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
-        System.out.println(department);
+
         Seller seller = new Seller(21, department, 3000.0, new Date(), "wellingtonflores@gmail.com", "Wellington Flores");
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
